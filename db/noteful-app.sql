55030b48185ddd9db3217eb2af47e36e495179ec
Clinton Owen <clint@clintonowen.com>
2018-08-08 12:41:44 -0500

    created noteful-app.sql

---
+++ db/noteful-app.sql
@@ -0,0 +1,82 @@
+-- psql -U dev -d noteful-app -f ./db/noteful-app.sql
+
+DROP TABLE IF EXISTS notes;
+DROP TABLE IF EXISTS folders;
+
+CREATE TABLE folders (
+    id serial PRIMARY KEY,
+    name text NOT NULL
+);
+
+ALTER SEQUENCE folders_id_seq RESTART WITH 100;
+
+INSERT INTO folders (name) VALUES
+  ('Archive'),
+  ('Drafts'),
+  ('Personal'),
+  ('Work');
+
+CREATE TABLE notes (
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  content text,
+  created timestamp DEFAULT now(),
+  folder_id int REFERENCES folders(id) ON DELETE SET NULL
+);
+
+ALTER SEQUENCE notes_id_seq RESTART WITH 1000;
+
+INSERT INTO notes (title, content, folder_id) VALUES
+  (
+    '5 life lessons learned from cats',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+    100
+  ),
+  (
+    'What the government doesn''t want you to know about cats',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a.',
+    100
+  ),
+  (
+    'The most boring article about cats you''ll ever read',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+    100
+  ),
+  (
+    '7 things lady gaga has in common with cats',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a.',
+    100
+  ),
+  (
+    'The most incredible article about cats you''ll ever read',
+    'Lorem ipsum dolor sit amet, boring consectetur adipiscing elit,',
+    100
+  ),
+  (
+    '10 ways cats can help you live to 100',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a.',
+    100
+  ),
+  (
+    '9 reasons you can blame the recession on cats',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+    100
+  ),
+  (
+    '10 ways marketers are making you addicted to cats',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a.',
+    100
+  ),
+  (
+    '11 ways investing in cats can make you a millionaire',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+    102
+  ),
+  (
+    'Why you should forget everything you learned about cats',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a.',
+    102
+  );
+
+-- -- get all notes
+-- SELECT * FROM notes;
